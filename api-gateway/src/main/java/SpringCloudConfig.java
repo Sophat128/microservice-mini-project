c8ff5ee452406c11ac1e35fd53e3783da9922ee9
Sophat <sophattann24@gmail.com>
2023-09-28 15:21:30 +0700

    update

---
+++ api-gateway/src/main/java/SpringCloudConfig.java
@@ -0,0 +1,15 @@
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator buildRouteLocator(RouteLocatorBuilder builder){
+       return builder.routes().route(r-> r.path("/price/**")
+                       .filters(f-> f.setResponseHeader("CustomRespHeader", "SomeValue"))
+                       .uri("http://localhost:8082"))
+                .route(r-> r.path("/inventory/**").uri("http://localhost:8083")).build();
+    }
+}
---
+++ src/test/java/com/example/microserviceminiproject/MicroserviceMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
