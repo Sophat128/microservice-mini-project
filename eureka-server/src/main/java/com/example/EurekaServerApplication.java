bf65db1fb131316f9c4041848f4c5ed66e992fe4
Sophat <sophattann24@gmail.com>
2023-09-28 18:38:47 +0700

    update config

--- eureka-server/src/main/java/com/example/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/EurekaServerApplication.java
@@ -1,14 +1,24 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServerApplication {
+public class EurekaServerApplication implements ApplicationRunner {
+    @Value("${greeting}")
+    private String greeting;
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
         System.out.println("Hello world!");
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("hello from : " + greeting);
     }
 }
