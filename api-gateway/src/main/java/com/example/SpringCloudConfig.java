dffa052624980b718f7178c4fff14bebe522ab93
Sophat <sophattann24@gmail.com>
2023-09-28 15:50:57 +0700

    update

--- api-gateway/src/main/java/com/example/SpringCloudConfig.java
+++ api-gateway/src/main/java/com/example/SpringCloudConfig.java
@@ -1,3 +1,5 @@
+package com.example;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
